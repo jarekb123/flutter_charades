54cedec96bc0ba02d6c370e101728c4debbeee82
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2020-04-13 22:16:36 +0200

    refactor record_scene_initializer_state.dart * rename to record_scene_bloc * leave bloc version only

--- lib/feature/record_scene/record_scene_bloc.dart
+++ lib/feature/record_scene/record_scene_bloc.dart
@@ -1,44 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:rxdart/subjects.dart';
 import 'package:kalambury/utils/rx_utils.dart';
 
-part 'record_scene_initializer_state.freezed.dart';
+part 'record_scene_bloc.freezed.dart';
 
-class RecordSceneInitializer extends ChangeNotifier {
-  static const _secondsToStart = 5;
-
-  final _remainingSeconds = BehaviorSubject<int>();
-  final _subscriptions = CompositeSubscription();
-
-  bool _initialized = false;
-
-  Stream<int> get remainingSeconds => _remainingSeconds;
-  bool get startRecording => _initialized;
-
-  @override
-  void dispose() {
-    _remainingSeconds.close();
-    super.dispose();
-  }
-
-  void init() {
-    _subscriptions.add(
-      Stream.periodic(
-        Duration(seconds: 1),
-        (elapsedSeconds) => _secondsToStart - elapsedSeconds,
-      ).take(_secondsToStart + 1).doOnDone(() {
-        _remainingSeconds.close();
-        _initialized = true;
-        notifyListeners();
-      }).listen(_remainingSeconds.add),
-    );
-  }
-}
-
-// BLOC version
 abstract class RecordSceneState {}
 
 @freezed
@@ -72,7 +38,7 @@ class OnStartRecordingSceneEvent implements RecordSceneEvent {}
 
 class RecordSceneBloc extends Bloc<RecordSceneEvent, RecordSceneState> {
   static const _initRemainingSeconds = 5;
-  static const _videoDuration = Duration(seconds: 30);
+  static const _videoDuration = Duration(seconds: 10);
 
   bool _initialized = false;
 
--- test/feature/record_scene/record_scene_state_test.dart
+++ test/feature/record_scene/record_scene_state_test.dart
@@ -1,29 +1,8 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kalambury/feature/record_scene/record_scene_initializer_state.dart';
+import 'package:kalambury/feature/record_scene/record_scene_bloc.dart';
 
 main() {
-  group('RecordSceneInitializer', () {
-    RecordSceneInitializer initializer;
-
-    setUp(() {
-      initializer = RecordSceneInitializer();
-    });
-
-    test(
-      'init() -> remainingSeconds emits 5..0 and then changes startRecording to true',
-      () async {
-        expect(initializer.startRecording, false);
-
-        initializer.init();
-
-        final remainingSeconds = await initializer.remainingSeconds.toList();
-        expect(initializer.startRecording, true);
-        expect(remainingSeconds, [5, 4, 3, 2, 1, 0]);
-      },
-    );
-  });
-
   group('RecordSceneInitBloc', () {
     var initEventExpectedStates = [
       ...[5, 4, 3, 2, 1, 0]
@@ -52,7 +31,7 @@ main() {
     );
 
     // TODO: Find out proper way to implement this test
-    // 
+    //
     // blocTest(
     //   'OnStartRecording event should stop recording after 30 seconds',
     //   build: () async => RecordSceneBloc(),
